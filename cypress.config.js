9fadc7e7b2960302da9fc89846c2002242ee6886
Eduferr <eduferr2803@gmail.com>
2025-11-08 17:47:17 -0300

    Implementa pipeline GitHub Actions

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl:'http://localhost:3000'
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'mochawesome-report',
+    overwrite: false,
+    reportFilename: 'index.html',
+    html: true,
+    json: true
+  }
 });
